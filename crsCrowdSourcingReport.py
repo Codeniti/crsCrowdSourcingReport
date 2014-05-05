308a5b50428a052fb7678c9e28dd064f10de9c2d
Tanay <tanaygahlot@gmail.com>
2014-05-05 12:01:23 +0530

    new version of app using bootstrap template and templating in python code

--- crsCrowdSourcingReport.py
+++ crsCrowdSourcingReport.py
@@ -1,171 +1,58 @@
 #oggpnosn
 #hkhr
 import webapp2
+from google.appengine.ext import ndb
+import jinja2
+import os
 
-webText="""<!DOCTYPE html>
-<html>
 
-<body>
-<h1 style="text-align:center;">Home Page</h1>
-<div align="center">
-<table style="text-align:center">
-<tr>
-  <td style="background-color:#C0C0C0"><a href="/manager">Manager</a></td>
-  </tr>
-<tr>
-  <td></td>
-</tr>
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
-<tr>
-  <td style="background-color:#C0C0C0"><a href=/organization>Organization</a></td>
-  </tr>
-<tr>
-  <td></td>
-</tr>
+class Task(ndb.Model):
+    problemStatement=ndb.StringProperty(indexed=False)
+    fund=ndb.FloatProperty()
+    contributor=ndb.JsonProperty(indexed=True)
+    status=ndb.BooleanProperty()
+    projectName=ndb.StringProperty(indexed=False)
+class Project(ndb.Model):
+    taskList=ndb.JsonProperty()
+    projectManager=ndb.StringProperty(indexed=False)
 
-</table>
-</div>
-</body>
 
-</html>
- """
-managerText="""<!DOCTYPE html>
-<html>
-
-<body>
-<h1 style="text-align:center;">Manager Dashboard</h1>
-<div align="middle">
-<table style="text-align:center">
-<tr>
-  <td style="background-color:#C0C0C0"><a href="/manager/newProject">Create New Project</a></td>
-  </tr>
-<tr>
-  <td></td>
-</tr>
-
-<tr>
-  <td style="background-color:#C0C0C0"><a href=/manager/updateProject>Update Existing Project</a></td>
-  </tr>
-<tr>
-  <td></td>
-</tr>
-
-</table>
-</div>
-</body>
-
-</html> """
-
-newProjectTextInitial="""<!DOCTYPE html>
-<html>
-
-<body>
-<h3 style="margin-left:50px">Add New Project</h1>
-<p style="margin-left:50px"><b>Enter Details Below</b></p>
-<div style="margin-left:50px">
-<form action="/manager/addProject/addTask" method="post">
-<fieldset>
-<p><b>Project Details</b></p><p>
-          <label>Project Name</label><br>
-          <textarea name="projectName" rows="1" cols="40">%s</textarea>
-</p>   
-<p>
-   <label>Project Description</label><br>
-   <textarea name="projectDescription" rows="4" cols="80">%s</textarea>
-</p>
-<p>
-   <label>Number Of Tasks</label><br>
-   <input type = "number" id = "numberOfTasks" name="noOfTasks"style="width:50px"/>
- </p>
-</fieldset><br>
- <input type="submit" value="Send"><input type="reset">
-</form>
-</div>
-</body>
-
-</html> """
-newProjectTextFinal=""" <!DOCTYPE html>
-<html>
-
-<body>
-<h3 style="margin-left:50px">Add New Project</h1>
-<p style="margin-left:50px"><b>Enter Details Below</b></p>
-<div style="margin-left:50px">
-<form action="" method="post">
-<fieldset>
-<p><b>Project Details</b></p><p>
-          <label>Project Name</label><br>
-          <textarea name="projectName" rows="1" cols="40">%s
-   </textarea>
-</p>   
-<p>
-   <label>Project Description</label><br>
-   <textarea name="projectDescription" rows="4" cols="80">%s
-   </textarea>
-</p>
-<p>
-   <label>Number Of Tasks</label><br>
-   <input type = "number" id = "numberOfTasks" style="width:50px"/>
- </p>
-</fieldset><br>
- """
-taskDetailText=""" <fieldset>
-<p><b>Task Details</b></p>
-<p>
-   <label>Description</label><br>
-<textarea name="desc" rows="2" cols="80"></textarea><br> 
-</p>
-<p>   <label>Fund Required</label><br>
-<input ty pe = "number" id = "fund" style="width:100px"/>
- </p>
-<p style="text-align:right"><a href="">Next</a></p>
-</fieldset><br>
-"""
 class MainPage(webapp2.RequestHandler):
-
     def get(self):
-        self.response.headers['Content-Type'] = 'text/html'
-        self.response.write(webText)
+        template = JINJA_ENVIRONMENT.get_template("homePage.html")
+        self.response.write(template.render({}))
 
-class Manager(webapp2.RequestHandler):
+class ProjectPage(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template("projectPage.html")
+        self.response.write(template.render({}))
 
-   def get(self):
-       self.response.headers['Content-Type'] = 'text/html'
-       self.response.write(managerText)
-        
-class Organization(webapp2.RequestHandler):
+class SignIn(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template("signInPage.html")
+        self.response.write(template.render({}))
 
-   def get(self):
-       self.response.headers['Content-Type'] = 'text/html'
-       self.response.write(webText)
+class ServicePage(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template("servicePage.html")
+        self.response.write(template.render({}))
+     
+class AboutPage(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template("aboutPage.html")
+        self.response.write(template.render({}))
 
-class NewProject (webapp2.RequestHandler):
-   def get(self):
-       self.response.headers['Content-Type'] = 'text/html'
-       self.response.write(newProjectTextInitial%("",""))
+class ContactPage(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template("contactPage.html")
+        self.response.write(template.render({}))
        
-class AddTask(webapp2.RequestHandler):
-   def get(self):
-       projectDescription=self.request.get("projectDescription")
-       projectName=self.request.get("projectName")
-       noOfTask=int(self.request.get("noOfTasks"))
-       self.response.write(newProjectTextFinal%(projectName,projectDescription))
-       for i in range(noOfTask):
-           self.response.write(taskDetailText)
-       self.response.write("""  <input type="submit" value="Send"><input type="reset">
-</FORM>
-</div>
-</body>
-</html>""")
-
-         
- 
-class UpdateProject (webapp2.RequestHandler):
-   def get(self):
-       self.response.headers['Content-Type'] = 'text/html'
-       self.response.write(webText)
-  
 
 application = webapp2.WSGIApplication([
-    ('/', MainPage),('/manager',Manager),('/organization',Organization),('/manager/newProject',NewProject),('/manager/updateProject',UpdateProject),("/manager/addProject/addTask",AddTask)
+    ('/', MainPage),('/signIn',SignIn),('/projects',ProjectPage),('/services',ServicePage),('/about',AboutPage),('/contact',ContactPage)
 ], debug=True)
